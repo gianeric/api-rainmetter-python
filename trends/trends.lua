cc090f57252385c0c22e0f5f228d82286e12243d
gianeric08 <gianeric08@gmail.com>
2024-07-05 00:13:04 -0300

    :recycle: delete logs

--- trends/trends.lua
+++ trends/trends.lua
@@ -1,34 +1,29 @@
 function Initialize()
-    print("Inicializando o script Lua para executar Python...")
     ExecutePython()
-    print("Script Python executado com sucesso.")
 end
 
 function Update()
-    print("Inicializando o script Lua para executar Python...")
     ExecutePython()
-    print("Script Python executado com sucesso.")
 
-    print("Atualizando...")
-    -- Leia o arquivo de texto
     local file = io.open("E:\\Projetos\\api-rainmetter-python\\trends\\trends.txt", "r")
     if file then
         local trends = file:read("*all")
         file:close()
-        print("Tendencias lidas com sucesso.")
         return trends
     end
-    print("Erro ao ler as tendencias.")
+
+    print("[UPDATE] Erro ao ler as tendencias.")
     return "Erro ao ler as tendencias"
 end
 
 function ExecutePython()
+    print("[START] Execute Python")
+
     local pythonExe = 'E:\\Projetos\\api-rainmetter-python\\env\\Scripts\\python.exe'
     local trendsScript = 'E:\\Projetos\\api-rainmetter-python\\trends\\trends.py'
 
-    -- Construir o comando Python sem abrir janela de prompt
     local command = string.format('start /B "" "%s" "%s" > NUL 2>&1', pythonExe, trendsScript)
-
-    -- local command = "@echo off E:\\Projetos\\api-rainmetter-python\\env\\Scripts\\python.exe E:\\Projetos\\api-rainmetter-python\\trends.py exit"
     os.execute(command)
+
+    print("[END] Execute Python")
 end
